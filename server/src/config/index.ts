1b0f0544e3145cb416bb569a5d1f2770c07400dc
XlipDev <xlip.studio@gmail.com>
2026-07-03 00:28:34 +0300

    feat: increase projectile cooldown to 4s for testing

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -23,7 +23,7 @@ export const DEFAULT_CONFIG: GameConfig = {
   speedStrafeFactor: 0.75,
   skills: {
     projectile: {
-      cooldown: 0.2,
+      cooldown: 4,
       projectileSpeed: 7.5,
       maxReach: 4,
       maxBurstParticles: 18,
